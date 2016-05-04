f7178f4044450a6dc1cd848b80445a25d86adfab
Igor Ł <igor.lapa@gmx.com>
2016-05-04 22:13:37 +0200

    some changes2

---
+++ src/animal/Animal.java
@@ -0,0 +1,10 @@
+package animal;
+
+/**
+ * Created by calm on 5/4/16.
+ */
+public class Animal {
+     void bark() {
+        System.out.println("Woof-Woof");
+    }
+}
---
+++ src/animal/AnimalMain.java
@@ -0,0 +1,11 @@
+package animal;
+
+/**
+ * Created by calm on 5/4/16.
+ */
+class AnimalMain {
+    public static void main (String[] args) {
+        Animal dog = new Animal();
+        dog.bark();
+    }
+}

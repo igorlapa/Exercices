c5f707edcb4a8f943f3aca1d93f6978937469104
Igor Ł <igor.lapa@gmx.com>
2016-05-04 22:24:59 +0200

    some changes2

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+/**
+ * Created by calm on 5/4/16.
+ */
+public class Vehicle {
+    int maxSpeed;
+    int wheels;
+    String colour;
+    double fuelCapacity;
+
+    void horn() {
+        System.out.println("Beep");
+    }
+}
+
+
